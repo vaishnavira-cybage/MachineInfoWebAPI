ea0efd8d2a888d545a0fce5160b0f10434df321d
Vaishnavi <vaishnavira@cybage.com>
2023-06-13 16:51:53 +0530

    initial commit

---
+++ MachineInfoWebAPI/Controllers/MachineInfoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MachineInfoWebAPI.Models;
+using System;
+
+namespace MachineInfoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineInfoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            MachineInfo machineDetails = new MachineInfo();
+
+            machineDetails.machineName = Environment.MachineName;
+            machineDetails.machineIP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            machineDetails.machineOS = Environment.OSVersion.VersionString;
+            machineDetails.timestamp = DateTime.Now.ToString();
+            return Ok(machineDetails);
+        }
+
+    }
+}
---
+++ MachineInfoWebAPI/Models/MachineInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MachineInfoWebAPI.Models
+{
+    public class MachineInfo
+    {
+        public string? machineName { get; set; }
+        public string? machineIP { get; set; }
+        public string? machineOS { get; set; }
+        public string? timestamp { get; set; }
+
+    }
+}
